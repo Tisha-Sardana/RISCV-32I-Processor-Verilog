4e4af8c531eb096e7ca7c0840230de160d321b2a
Tisha-Sardana <tishasardana573@gmail.com>
2026-08-11 13:02:57 +0530

    Clean up comments in testbench.v
    Removed commented header information from testbench.v

--- testbench.v
+++ testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.01.2026 00:56:41
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench();
     reg         clk, reset;
