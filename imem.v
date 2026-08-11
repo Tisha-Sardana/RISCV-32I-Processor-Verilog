7819e3bec94473e1fcb43bcd9181eae7718ec119
Tisha-Sardana <tishasardana573@gmail.com>
2026-08-11 12:10:10 +0530

    Update imem.v

--- imem.v
+++ imem.v
@@ -5,7 +5,7 @@ module imem(
     reg [31:0] RAM[63:0];
 
     initial begin
-        // We initialize the memory manually here
+        
         RAM[0] = 32'h200093; // addi x1, x0, 2  (x1 = 2)
         RAM[1] = 32'h200113; // addi x2, x0, 2  (x2 = 2)
         RAM[2] = 32'h002081b3; // add x3, x1, x2 (x3 = 4)
